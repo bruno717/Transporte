6fc2ea18dfbead9bb3a46f5bac420de5b7b798df
bruno717 <bruno.oliveira@redspark.io>
2017-10-01 20:01:32 -0300

    visao do mapa metropolitano horizontal

--- android/MeuMetro/app/src/main/java/br/com/bruno/meumetro/TransportMapActivity.java
+++ android/MeuMetro/app/src/main/java/br/com/bruno/meumetro/TransportMapActivity.java
@@ -1,11 +1,13 @@
 package br.com.bruno.meumetro;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,6 +20,8 @@ public class TransportMapActivity extends AppCompatActivity {
 
     @Bind(R.id.meu_metro_toolbar)
     Toolbar mToolbar;
+    @Bind(R.id.activity_transport_map_web_view)
+    WebView mWebView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,13 +30,25 @@ public class TransportMapActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupToolbar();
+        setupWebView();
     }
+
 
     private void setupToolbar() {
         mToolbar.setNavigationIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.md_nav_back));
         setSupportActionBar(mToolbar);
         if (getSupportActionBar() != null)
             getSupportActionBar().setTitle(R.string.activity_transport_map_toolbar_title);
+    }
+
+    private void setupWebView() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mWebView.getSettings().setUseWideViewPort(true);
+            mWebView.getSettings().setBuiltInZoomControls(true);
+            mWebView.getSettings().setDisplayZoomControls(false);
+            mWebView.getSettings().setLoadWithOverviewMode(true);
+            mWebView.loadUrl("file:///android_asset/transport_map.html");
+        }
     }
 
     @Override
