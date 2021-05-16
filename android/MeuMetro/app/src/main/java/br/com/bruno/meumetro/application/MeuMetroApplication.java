10a9e16b269acd5aa574052b2438f1d7b36eecfb
unknown <bruno.oliveira@redspark.io>
2021-05-15 22:02:50 -0300

    ajuste para pegar token de notificacao dos dispositivos que nao foram migrados no banco de dados

--- android/MeuMetro/app/src/main/java/br/com/bruno/meumetro/application/MeuMetroApplication.java
+++ android/MeuMetro/app/src/main/java/br/com/bruno/meumetro/application/MeuMetroApplication.java
@@ -1,18 +1,20 @@
 package br.com.bruno.meumetro.application;
 
 import android.content.Context;
+import android.provider.Settings;
 import android.support.multidex.MultiDexApplication;
 
 import com.blankj.utilcode.util.Utils;
 import com.crashlytics.android.Crashlytics;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.io.IOException;
 
 import br.com.bruno.meumetro.BuildConfig;
 import br.com.bruno.meumetro.database.MigrationMyDataBase;
-import br.com.bruno.meumetro.enums.ConnectionType;
 import br.com.bruno.meumetro.managers.SharedPreferenceManager;
 import br.com.bruno.meumetro.models.Device;
 import br.com.bruno.meumetro.rest.DeviceService;
-import br.com.bruno.meumetro.rest.managers.RestManager;
 import io.fabric.sdk.android.Fabric;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -50,6 +52,25 @@ public class MeuMetroApplication extends MultiDexApplication {
         Device device = SharedPreferenceManager.getDeviceToken();
         if (device != null) {
             new DeviceService().saveTokenDevice(device, true);
+        } else {
+            // Lógica necessária para guardar token dos dispositivos que nao tem mais registro do token no banco após a migracao 15/05/2021
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        String idDevice = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+                        final Device device = new Device();
+                        String senderId = "932522839298";
+                        String token = FirebaseInstanceId.getInstance().getToken(senderId, "FCM");
+                        device.setIdDevice(idDevice);
+                        device.setTokenDevice(token);
+                        SharedPreferenceManager.saveDeviceToken(device);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
         }
     }
 }
